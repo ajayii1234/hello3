53b418ec1e69731639753483cebeda3af38512b6
ajayii1234 <ajayi.femi.eee@gmail.com>
2024-05-21 15:14:21 +0100

    3rd change

---
+++ src/AnotherComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AnotherComponent = () => {
+  return (
+    <div>
+      <h2>Welcome to Another Component!</h2>
+      <p>This is another simple React component.</p>
+    </div>
+  );
+};
+
+export default AnotherComponent;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MyComponent from './MyComponent';
+import AnotherComponent from './AnotherComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" width="70px" height="70px" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello world, I love React. It is the best
-          I am a programmer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="App">
+          <MyComponent/>
+        </div>
+        <div>
+          <AnotherComponent />
+        </div>
+    </>
   );
 }
 
